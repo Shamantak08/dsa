18579349fa72e8576bd70e0deecb4210058bc4e8
Shamantak Bhattacharyya <shamantak.g7@gmail.com>
2026-08-25 07:58:48 +0530

    Corrected program logic

--- subarray.java
+++ subarray.java
@@ -3,11 +3,33 @@ public class subarray {
     public static void main(String args[]){
         Scanner sc = new Scanner(System.in);
 
-        int[] array = {1,2,3};
+        System.out.print("Number of elements: ");
+        int n = sc.nextInt();
 
-        for (int i = 0 ; i < array.length ; i++){
-            for (int j = i ; j < array.length ; j++){
-                System.out.print(array[i]);
+        int[] array = new int[n];
+
+        for (int i = 0 ; i<n ; i++){
+            System.out.print("Enter element " + (i+1) + ": ");
+            array[i] = sc.nextInt();
+        }
+
+        System.out.println("");
+
+        //printing original array
+
+        System.out.print("Given array: ");
+        
+        for (int i = 0 ; i<n ; i++){
+            System.out.print(array[i] + " ");
+        }
+        System.out.println();
+
+        for (int si = 0 ; si < array.length ; si++){
+            for (int ei = si ; ei < array.length ; ei++){
+                for (int k = si ; k <= ei ; k++){
+                    System.out.print(array[k] + " ");
+                }
+                System.out.println();
             }
         }
     }
